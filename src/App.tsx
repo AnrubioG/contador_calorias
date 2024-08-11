cc043361886a1e2e43e55f1090917851c06d79fb
AnubioG <angela.rubio@genstudents.org>
2024-08-11 18:44:42 -0500

    Bootón para restablecer todas las actividades

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { useReducer, useEffect, useMemo } from "react";
 import Form from "./components/Form";
 import { activityReducer, initialState } from "./reducers/activity-reducer";
 import ActivityList from "./components/ActivityList";
@@ -6,13 +6,27 @@ import ActivityList from "./components/ActivityList";
 function App() {
   const [state, dispatch] = useReducer(activityReducer, initialState);
 
+  useEffect(() => {
+    localStorage.setItem("activities", JSON.stringify(state.activities));
+  }, [state.activities]);
+
+  const canRestartApp = () =>
+    useMemo(() => state.activities.length > 0, [state.activities]);
+
   return (
     <>
       <header className="bg-lime-600 py-3">
-        <div className="max-w-4xl mx-auto flex justify-between">
+        <div className="max-w-4xl mx-auto px-4 flex justify-between">
           <h1 className="text-center text-lg font-bold text-white uppercase">
             Contador de Calorias
           </h1>
+          <button
+            className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10"
+            disabled={!canRestartApp()}
+            onClick={() => dispatch({ type: "restart-app" })}
+          >
+            Reiniciar App
+          </button>
         </div>
       </header>
 
